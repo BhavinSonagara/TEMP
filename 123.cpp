2e3b0444dccd63434ae867b5902062cfb397c941
BhavinSonagara <81000109+BhavinSonagara@users.noreply.github.com>
2023-09-16 22:16:01 +0530

    Update 123.cpp conflict

--- 123.cpp
+++ 123.cpp
@@ -7,6 +7,13 @@ void solve()
     int n;
     cin >> n;
     n = n + 1;
+
+      int n;
+    cin >> n;
+    n = n + 1;
+      int n;
+    cin >> n;
+    n = n + 1;
 }
 int main() {
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
