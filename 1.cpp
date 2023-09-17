227d8ce4cb077ba0c1ac12b9daf4c2fec8bf8e9c
BhavinSonagara <bhavinsonagara2@gmail.com>
2023-09-17 12:09:06 +0530

    commit for rebase main

--- 1.cpp
+++ 1.cpp
@@ -6,41 +6,8 @@ void solve()
 {
     int n;
     cin >> n;
-    n = n - 1;
+    n = n+1;
 }
-
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define mod 1000000007
-void solve()
-{
-    int n;
-    cin >> n;
-    n = n + 1;
-}
-int main() {
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-//#ifndef ONLINE_JUDGE
-//	freopen("input.txt", "r", stdin);
-//	freopen("output.txt", "w", stdout);
-//#endif
-
-
-//#ifndef ONLINE_JUDGE
-//	freopen("input.txt", "r", stdin);
-//	freopen("output.txt", "w", stdout);
-//#endif
-
-    ll test=1;
-    cin>>test;
-    while(test--)
-    {
-        solve();
-    }
-    return 0;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 //#ifndef ONLINE_JUDGE
